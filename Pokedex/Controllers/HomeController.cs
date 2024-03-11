f0bf650569b19d6da43e6020c3545eddb2f48c98
guxtagrimes <gustavolor190@gmail.com>
2024-03-11 16:43:47 -0300

    HomeController.cs

--- Pokedex/Controllers/HomeController.cs
+++ Pokedex/Controllers/HomeController.cs
@@ -32,6 +32,29 @@ public class HomeController : Controller
         return View(pokemons); 
     }
 
+    public IActionResult Details(int id)
+    {
+        List<Pokemon> pokemons = new();
+        using (StreamReader leitor = new("Data\\pokemons.json"))
+        {
+            string dados = leitor.ReadToEnd();
+            pokemons = JsonSerializer.Deserialize<List<Pokemon>>(dados);
+        }
+        List<Tipo> tipos = new();
+        using (StreamReader leitor = new("Data\\tipos.json"))
+        {
+            string dados = leitor.ReadToEnd();
+            tipos = JsonSerializer.Deserialize<List<Tipo>>(dados);
+        }
+        DetailsVM details = new() {
+            Tipos = tipos,
+            Atual = pokemons.FirstOrDefault(p => p.Numero == id),
+            Anterior = pokemons.OrderByDescending(p => p.Numero).FirstOrDefault(p => p.Numero < id),
+            Proximo = pokemons.OrderBy(p => p.Numero).FirstOrDefault(p => p.Numero > id),
+        };
+        return View(details);
+    }   
+
     public IActionResult Privacy()
     {
         return View();
