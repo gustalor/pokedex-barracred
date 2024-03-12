37747b4c04c9ed3538b0771255fc4e6bf4775bf0
guxtagrimes <gustavolor190@gmail.com>
2024-03-12 14:18:34 -0300

    Projeto finalizado Pokedex

--- Pokedex/Models/DetailsVM.cs
+++ Pokedex/Models/DetailsVM.cs
@@ -5,5 +5,5 @@ public class DetailsVM
     public Pokemon Anterior { get; set; }
     public Pokemon Atual { get; set; }
     public Pokemon Proximo { get; set; }
-    public List<Tipo> Tipos {get; set; }
+    public List<Tipo> Tipos { get; set; }
 }
